36b58dec125244ed3b38fc3d0fd5175c6f39ad8f
Ben Wu <benwu@im.fju.edu.tw>
2025-03-12 19:20:37 +0800

    next預設內容

---
+++ app/page.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+  <div className={styles.page}>
+    <main className={styles.main}>
+    <h1>FJU Go Go</h1>
+    <ul>
+      <li><Link href="/test">Test</Link></li>
+      <li><a href="http://www.im.fju.edu.tw">輔大資管</a></li>
+    </ul>
+    </main>
+  </div>
+  )
+}
