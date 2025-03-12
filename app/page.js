cda40ce7246e85336299d224ccaa0715ecbcb0f8
黎小雅 <413155094@m365.fju.edu.tw>
2025-03-12 21:23:26 +0800

    test

--- app/page.js
+++ app/page.js
@@ -7,8 +7,8 @@ export default function Home() {
     <main className={styles.main}>
     <h1>FJU Go Go</h1>
     <ul>
-      <li>吳濟聰</li>
-      <li><Link href="/test">Test</Link></li>
+      <li>吳濟聰</li>黎小雅
+            <li><Link href="/test">Test</Link></li>
       <li><a href="http://www.im.fju.edu.tw">輔大資管</a></li>
     </ul>
     </main>
