8866683d083a4e590150c7d512859f94a2cbdfb0
Ben Wu <benwu@im.fju.edu.tw>
2025-03-30 16:38:50 +0800

    #1 加上授課教師

--- app/course.js
+++ app/course.js
@@ -3,11 +3,13 @@ import { getFirestore, collection, getDocs } from "firebase/firestore";
 import app from "@/app/_firebase/Config";
 const db = getFirestore(app);
 const querySnapshot = await getDocs(collection(db, "系所課程"));
-console.log("系所課程資料：");
+// console.log("系所課程資料：");
 let courses = [];
 querySnapshot.forEach((doc) => {
-  console.log(`${doc.id} => ${doc.data().課程名稱}`);
+  // console.log(`${doc.id} => ${doc.data().課程名稱}`);
   courses.push(doc.data());
+  // console.log(`${doc.id} => ${doc.data().教師.join("、")}`);
+
 });
 
 //<body> is moved to layout.js
@@ -23,14 +25,16 @@ export default function Course() {
       <div className="section">
         <div className={styles.main}>
           <h2>修業規則與必選修課程</h2>
-          <table class="course-table">
+          <table className="course-table">
             <tr>
               <th>課程名稱</th>
+              <th>授課教師</th>
             </tr>
             
             {courses.map((course, index) => (
             <tr key={index}>
               <td>{course.課程名稱}</td>
+              <td>{course.教師.join("、")}</td>
             </tr>
           ))}
           </table>
