644b61dc3d75533fdfcf0bdb5cd18ba9676deb3a
Ben Wu <benwu@im.fju.edu.tw>
2025-04-24 00:10:02 +0800

    #18 明天再研究

--- app/teacher/[tid]/page.js
+++ app/teacher/[tid]/page.js
@@ -1,5 +1,15 @@
+
+export async function generateStaticParams() {
+  // Replace with your actual dynamic parameters
+  return [
+    { tid: '1' },
+    { tid: '2' },
+    { tid: 'example' },
+  ];
+}
+
 export default async function Page({ params }) {
-  const { tid } = await params
+  const { tid } = params;
   const tname = isNaN(Number(tid)) ? decodeURIComponent(tid) : tid;
-  return <div>tname: {tname}</div>
+  return <div>tname: {tname}</div>;
 }
