122c487c6829fa654b2bb51ac2cc5952fcaf3263
Ben Wu <benwu@im.fju.edu.tw>
2025-04-24 07:26:11 +0800

    #18 解決github pages錯誤

--- app/teacher/[tid]/page.js
+++ app/teacher/[tid]/page.js
@@ -1,14 +1,14 @@
 import { getFirestore, collection, getDocs } from 'firebase/firestore';
 import app from "@/app/_firebase/Config";
 
-export async function generateStaticParams() {
+export const generateStaticParams = async () => {
   // Replace with your actual dynamic parameters
   const db = getFirestore(app);
   const teacherCollection = collection(db, '系所師資');
   const snapshot = await getDocs(teacherCollection);
   const params = snapshot.docs.map(doc => ({ tid: doc.id }));
   return params;
-}
+};
 
 export default async function Page({ params }) {
   const { tid } = await params;
