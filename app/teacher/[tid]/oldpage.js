bcac29b68e2bdb28c43948601bc169590cace684
Ben Wu <benwu@im.fju.edu.tw>
2025-04-24 00:02:06 +0800

    #18 回到基本作法，明天繼續研究

---
+++ app/teacher/[tid]/oldpage.js
@@ -0,0 +1,63 @@
+'use client'
+// 被copilot騙了，use client不能用這種寫法
+// import { useParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { getFirestore, getDoc, doc, collection, query, where, getDocs } from 'firebase/firestore';
+import app from "@/app/_firebase/Config";
+
+export default function Page({params}) {
+  const [teacherData, setTeacherData] = useState(null);
+  const [tid, setTid] = useState('053792');
+  const [tName, setTName] = useState(null);
+  const db = getFirestore(app);
+  async function getParams() {
+    const { tid } = await params;
+    setTid(tid); // 更新 tid 狀態
+    return tid; // 返回 tid 和 tname
+  }
+  setTid(getParams()); // 從路由參數中獲取tid和tname
+
+  useEffect(() => {
+
+    async function fetchTeacherData() {
+      try {
+        console.log("tid:",tid);
+        if (tid==tname) {
+          //如果tid是數字，則直接使用tid查詢
+          const querySnapshot = await getDoc(doc(db, '系所教師', tid));
+          if (querySnapshot.exists()) {
+            setTeacherData(querySnapshot.data());
+          } else {
+            console.log('No matching documents!');
+          }
+        }else{
+          //如果tid是名字，則直接使用名字查詢
+          const querySnapshot = await getDocs(query(collection(db, '系所教師'), where('姓名', '==', tname)));
+          if (!querySnapshot.empty) {
+            querySnapshot.forEach((doc) => {
+              setTeacherData(doc.data());
+            });
+          } else {
+            console.log('No matching documents!');
+          }
+        }
+      } catch (error) {
+        console.error('Error fetching teacher data:', error);
+      }
+
+
+    }
+    const tname = isNaN(Number(tid)) ? decodeURIComponent(tid) : tid;
+
+    fetchTeacherData();
+  }, [tid]);
+  
+  return teacherData?
+    <div>
+      <p>姓名: {teacherData.姓名}</p>
+      <p>電子郵件: {teacherData.信箱}</p>
+      <p>辦公室位置: {teacherData.辦公室位置}</p>
+    </div>
+    :
+    <p>查無資料</p>;
+}
