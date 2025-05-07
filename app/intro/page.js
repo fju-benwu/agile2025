b6376b6bfc6fd242c0e8c4a26dce1e101c9e80b7
WuHsinPei <413145142@m365.fju.edu.tw>
2025-05-07 21:10:40 +0800

    系所介紹的頁面

---
+++ app/intro/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <div className="container mx-auto p-4">
+            <h1 className="text-2xl font-bold">Welcome to the Intro Page</h1>
+            <p className="mt-4">Hello</p>
+        </div>
+    );
+}
