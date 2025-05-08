326553a61941acfff87d765a3fb5d913237a3ead
Ben Wu <benwu@im.fju.edu.tw>
2025-05-09 07:36:30 +0800

    #27 改用next/Image，提升效率

--- app/intro/page1.js
+++ app/intro/page1.js
@@ -1,15 +1,19 @@
 import '../globals.css';
+import Image from 'next/image';
 
 export default function Page1() {
     return (
-        <div >
+        <div>
             <h1>Welcome to Page 1</h1>
             <p>This is the content of Page 1.</p>
-            <img width="80%"
-            style={{ display: 'block', margin: 'auto' }} 
-                src="https://www.im.fju.edu.tw/wp-content/uploads/2024/03/19-1.jpg" 
-                alt="輔仁資管所圖片" 
-            />
+            <div style={{ width: '80%', margin: 'auto'}}>
+                <Image 
+                    src="https://www.im.fju.edu.tw/wp-content/uploads/2024/03/19-1.jpg" 
+                    alt="輔仁資管所圖片" 
+                    width={800} // 設定圖片寬度
+                    height={450} // 設定圖片高度，保持比例
+                />
+            </div>
         </div>
     );
 }
