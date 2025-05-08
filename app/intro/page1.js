5d811b47a82841d9594667000d2265a09b66d0b7
Ben Wu <benwu@im.fju.edu.tw>
2025-05-09 07:49:27 +0800

    #27 依照copilot的建議，利用Image的layout維持原本的長寬比

--- app/intro/page1.js
+++ app/intro/page1.js
@@ -9,7 +9,8 @@ export default function Page1() {
             <div >
                 <Image style={{ width: '80%', display:'block',  margin: 'auto'}} 
                     src="https://www.im.fju.edu.tw/wp-content/uploads/2024/03/19-1.jpg" 
-                    alt="輔仁資管所圖片" 
+                    alt="輔仁資管所圖片"
+                    layout="intrinsic" // 維持圖片的原始長寬比 
                     width={800} // 設定圖片寬度
                     height={450} // 設定圖片高度，保持比例
                 />
