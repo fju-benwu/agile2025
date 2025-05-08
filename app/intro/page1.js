83d21db7e2917ed2bdc4ce4b4d20b756cc474901
Ben Wu <benwu@im.fju.edu.tw>
2025-05-09 07:27:14 +0800

    #27 處理圖片大小並置中

--- app/intro/page1.js
+++ app/intro/page1.js
@@ -1,13 +1,15 @@
+import '../globals.css';
+
 export default function Page1() {
     return (
-        <div className="container mx-auto p-4">
-            <h1 className="text-2xl font-bold">Welcome to Page 1</h1>
-            <p className="mt-4">This is the content of Page 1.</p>
-            <img 
+        <div >
+            <h1>Welcome to Page 1</h1>
+            <p>This is the content of Page 1.</p>
+            <img width="80%"
+            style={{ display: 'block', margin: 'auto' }} 
                 src="https://www.im.fju.edu.tw/wp-content/uploads/2024/03/19-1.jpg" 
                 alt="輔仁資管所圖片" 
-                className="mt-4 rounded shadow-lg"
             />
-        </div>//
+        </div>
     );
 }
