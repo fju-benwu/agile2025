b552b93008b36da9295860ac3f1f40dade78cce0
黎小雅 <413155094@m365.fju.edu.tw>
2025-05-09 00:15:58 +0800

    新增照片:開箱資管所

---
+++ app/intro/page1.js
@@ -0,0 +1,13 @@
+export default function Page1() {
+    return (
+        <div className="container mx-auto p-4">
+            <h1 className="text-2xl font-bold">Welcome to Page 1</h1>
+            <p className="mt-4">This is the content of Page 1.</p>
+            <img 
+                src="https://www.im.fju.edu.tw/wp-content/uploads/2024/03/19-1.jpg" 
+                alt="輔仁資管所圖片" 
+                className="mt-4 rounded shadow-lg"
+            />
+        </div>//
+    );
+}
