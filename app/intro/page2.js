4d59019b06b66ab54addbe1fc4e59300d9a991ad
吳玟慧 <bright6253@gmail.com>
2025-05-14 19:10:16 +0800

    test

--- app/intro/page2.js
+++ app/intro/page2.js
@@ -2,7 +2,7 @@ export default function Page2() {
     return (
         <div className="p-4 text-center">
             <h1 className="text-2xl font-bold">這是 Page2</h1>
-            <p className="mt-2">你可以在這裡放一些文字或圖片</p>
+            <p className="mt-2">hello</p>
         </div>
     );
 }
