4c79dac1d91252079ac5af36108749b1f28689c8
WuHsinPei <413145142@m365.fju.edu.tw>
2025-05-14 21:30:20 +0800

    更新page0(照片配合版面80%)

--- app/intro/page0.js
+++ app/intro/page0.js
@@ -7,6 +7,7 @@ export default function Page1() {
             <p></p>
             <div>
                 <img 
+                    style={{ width: '80%', margin: '20px auto', display: 'block' }} 
                     src="https://www.im.fju.edu.tw/wp-content/uploads/2024/03/19-1.jpg" 
                     alt="輔仁資管所圖片" 
                     className="mt-4 rounded shadow-lg"
