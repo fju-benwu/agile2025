5cabde5c504302b7b21868a78eef492449ac94ea
Ben Wu <benwu@im.fju.edu.tw>
2025-05-14 19:09:25 +0800

    #27 新增page2內容

--- app/intro/page2.js
+++ app/intro/page2.js
@@ -1,8 +1,8 @@
 export default function Page2() {
     return (
         <div className="p-4 text-center">
-            <h1 className="text-2xl font-bold">這是 Page2</h1>
-            <p className="mt-2">你可以在這裡放一些文字或圖片</p>
+            <h1 className="text-2xl font-bold">輔大資管系</h1>
+            <p className="mt-2">是一個溫暖的大家庭</p>
         </div>
     );
 }
