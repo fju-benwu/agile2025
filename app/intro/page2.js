a0dbb943ab76e309a865a3ff54e4bfd732e2b4c8
WuHsinPei <413145142@m365.fju.edu.tw>
2025-05-14 21:02:21 +0800

    增加page2 圖片

--- app/intro/page2.js
+++ app/intro/page2.js
@@ -3,6 +3,11 @@ export default function Page2() {
         <div className="p-4 text-center">
             <h1 className="text-2xl font-bold">強大師資內容</h1>
             <p className="mt-2">輔大資管所師資優秀多元，結合理論與實務，涵蓋人工智慧、大數據、系統分析等領域。教授具備豐富教學與產學經驗，引導學生掌握科技趨勢，培養實作與研究能力。無論轉職科技產業或持續深造，都是您的最佳選擇。</p>
+        <img 
+                src="https://www.im.fju.edu.tw/wp-content/uploads/2024/03/line_oa_chat_221130_163602_group_8-e1711008660828.jpg" 
+                alt="輔仁資管所師生照" 
+                className="mt-4 rounded shadow-lg"
+            />
         </div>
     );
 }
