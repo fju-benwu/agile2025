3e472e2562b7f6cdf2a48786f995984b0cb2a5d2
WuHsinPei <413145142@m365.fju.edu.tw>
2025-05-14 20:58:25 +0800

    更新page2內容及修正page3檔名

--- app/intro/page2.js
+++ app/intro/page2.js
@@ -1,7 +1,7 @@
 export default function Page2() {
     return (
         <div className="p-4 text-center">
-            <h1 className="text-2xl font-bold">強大師資陣容</h1>
+            <h1 className="text-2xl font-bold">強大師資內容</h1>
             <p className="mt-2">輔大資管所師資優秀多元，結合理論與實務，涵蓋人工智慧、大數據、系統分析等領域。教授具備豐富教學與產學經驗，引導學生掌握科技趨勢，培養實作與研究能力。無論轉職科技產業或持續深造，都是您的最佳選擇。</p>
         </div>
     );
