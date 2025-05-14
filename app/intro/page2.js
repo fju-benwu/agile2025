fb0abb33772c5a30f69c3d4c2a6cbb026e8016fc
WuHsinPei <413145142@m365.fju.edu.tw>
2025-05-14 20:40:22 +0800

    page2修改

--- app/intro/page2.js
+++ app/intro/page2.js
@@ -1,8 +1,8 @@
 export default function Page2() {
     return (
         <div className="p-4 text-center">
-            <h1 className="text-2xl font-bold">這是 Page2</h1>
-            <p className="mt-2">你可以在這裡放一些文字或圖片</p>
+            <h1 className="text-2xl font-bold">強大師資陣容</h1>
+            <p className="mt-2">輔大資管所師資優秀多元，結合理論與實務，涵蓋人工智慧、大數據、系統分析等領域。教授具備豐富教學與產學經驗，引導學生掌握科技趨勢，培養實作與研究能力。無論轉職科技產業或持續深造，都是您的最佳選擇。</p>
         </div>
     );
 }
