097f3c67d73217780a5c4afc694c167a204e8007
WuHsinPei <413145142@m365.fju.edu.tw>
2025-05-21 21:07:15 +0800

    更新yt顯示尺寸

--- app/intro/page3.js
+++ app/intro/page3.js
@@ -3,15 +3,27 @@ export default function Page3() {
         <div className="container mx-auto p-4">
             <h1 className="text-2xl font-bold mb-6 text-center">輔大資管搶先看</h1>
             <div className="flex justify-center">
-                <iframe
-                    width="560"
-                    height="315"
-                    src="https://www.youtube.com/embed/KCA4jxwc2Pg"
-                    title="YouTube video player"
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowFullScreen
-                ></iframe>
+                <div style={{
+                    position: "relative",
+                    width: "100%",
+                    maxWidth: "800px",
+                    aspectRatio: "16 / 9",
+                }}>
+                    <iframe
+                        src="https://www.youtube.com/embed/KCA4jxwc2Pg"
+                        title="YouTube video player"
+                        style={{
+                            position: "absolute",
+                            top: 0,
+                            left: 0,
+                            width: "100%",
+                            height: "100%",
+                            border: 0
+                        }}
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowFullScreen
+                    ></iframe>
+                </div>
             </div>
         </div>
     );
